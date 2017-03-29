d078f528b31fb0304ac482939bc9d137a5597e57
cheerqjy <cheerqjy@163.com>
2017-03-29 12:34:20 +0800

    vue测试

---
+++ src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+	<div><h3>我是主页</h3></div>
+</template>
---
+++ src/components/News.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h3>我是新闻</h3>
+		<ul>
+			<li v-for="(arr,index) in newsData">{{index}}:{{arr}}</li>
+		</ul>
+	</div>
+</template>
+<script>
+export default {
+	name:'news',
+	data(){
+		return {
+			newsData:[111,222,333,444]
+		}
+	}
+}
+</script>
